d2490975e66ab82ce043f88eaf5cb97d8d4cf2dc
Mateusz Jakub <mateusz.jaakub@gmail.com>
2022-02-20 22:33:24 +0100

    Initial commit

---
+++ src/main/java/pl/nullpointerexception/restapi/config/LoginCredentials.java
@@ -0,0 +1,9 @@
+package pl.nullpointerexception.restapi.config;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/pl/nullpointerexception/restapi/service/PostService.java
@@ -0,0 +1,66 @@
+package pl.nullpointerexception.restapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import pl.nullpointerexception.restapi.model.Comment;
+import pl.nullpointerexception.restapi.model.Post;
+import pl.nullpointerexception.restapi.repository.CommentRepository;
+import pl.nullpointerexception.restapi.repository.PostRepository;
+
+import javax.transaction.Transactional;
+import java.beans.Transient;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private static final int PAGE_SIZE = 20;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+
+    public List<Post> getPosts(int page, Sort.Direction sort) {
+        return postRepository.findAllPosts(PageRequest.of(page, PAGE_SIZE, Sort.by(sort, "id")));
+    }
+
+    public Post getSinglePost(long id) {
+        return postRepository.findById(id)
+                .orElseThrow();
+    }
+
+    public List<Post> getPostsWithComments(int page, Sort.Direction sort) {
+        List<Post> allPosts = postRepository.findAllPosts(PageRequest.of(page, PAGE_SIZE, Sort.by(sort, "id")));
+        List<Long> ids = allPosts.stream()
+                .map(Post::getId)
+                .collect(Collectors.toList());
+        List<Comment> comments = commentRepository.findAllByPostIdIn(ids);
+        allPosts.forEach((post -> post.setComment(extractComments(comments, post.getId()))));
+        return allPosts;
+    }
+
+    private List<Comment> extractComments(List<Comment> comments, long id) {
+        return comments.stream()
+                .filter(comment -> comment.getPostId() == id)
+                .collect(Collectors.toList());
+    }
+
+    public Post addPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Transactional
+    public Post editPost(Post post) {
+        Post postEdited = postRepository.findById(post.getId()).orElseThrow();
+        postEdited.setTitle(post.getTitle());
+        postEdited.setContent((post.getContent()));
+        return postEdited;
+    }
+
+    public void deletePost(long id) {
+        postRepository.deleteById(id);
+    }
+}
